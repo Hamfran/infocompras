11f76d4f2eefc5a683330be2fb88f76fb9ef8556
HendrixDev13 <mendozahendrix9@gmail.com>
2024-10-15 00:53:39 -0600

    nuevas vistas

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
 const express = require('express');
-<<<<<<< HEAD
-const { connectToDB } = require('./config/conexionBD'); // Importamos la conexión a la base de datos
-const clienteRoutes = require('./routers/clientes');
-const ProvedorRoutes = require('./routers/Proveedor');
-=======
 const { poolPromise } = require('./config/conexionBD'); // Importamos la conexión a la base de datos
 const clienteRoutes = require('./routers/Usuarios');
 const departamentosRoutes = require('./routers/Departamentos'); // rutas para departamentos
->>>>>>> b578b57c0610dfb3745813207317cc06ac5b996e
 const dotenv = require('dotenv');
 
 
@@ -16,12 +10,6 @@ const port = process.env.PORT || 3000;
 
 // Middleware para procesar datos en formato JSON
 app.use(express.json());
-<<<<<<< HEAD
-//rutasconst clienteRoutes = require('./routers/clientes');
-app.use(express.static('Manager'));
-app.use('/Proveedor', ProvedorRoutes);
-app.use('/clientes', clienteRoutes);
-=======
 
 // Middleware para servir archivos estáticos desde la carpeta 'public'
 app.use(express.static('Manager')); // Aquí estamos sirviendo la carpeta 'Manager'
@@ -30,7 +18,6 @@ app.use('/Usuarios', clienteRoutes);
 
 app.use('/Departamentos', departamentosRoutes); // Agregar esta línea para los departamentos
 
->>>>>>> b578b57c0610dfb3745813207317cc06ac5b996e
 // Definir una ruta simple
 app.get('/', (req, res) => {
     res.send('¡Hola! Conexión a SQL Server funcionando.');
